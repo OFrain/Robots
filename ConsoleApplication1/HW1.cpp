40f53e3a7584cfc4e5c8439bd1da448e2093ca30
OFrain <onf32955@uga.edu>
2023-02-10 00:40:31 -0500

    push

--- ConsoleApplication1/HW1.cpp
+++ ConsoleApplication1/HW1.cpp
@@ -1,6 +1,11 @@
 
-#include <opencv2/opencv.hpp>
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <opencv2/ml.hpp>
 #include <iostream>
+
+#include <vector>
+
 
 using namespace cv;
 using namespace std;
@@ -9,39 +14,7 @@ Mat image, gray;
 Mat output, output_norm, output_norm_scaled;
 
 void harrisCornerDetector() {
-    //Opens orginal image
-    //cv::imshow("Original image", image);
 
-    // Converting the color image into grayscale
-    cvtColor(image, gray, cv::COLOR_BGR2GRAY);
-
-    // Detecting corners using the cornerHarris built in function
-    output = Mat::zeros(image.size(), CV_32FC1);
-    cornerHarris(gray, output,
-        3,              // Neighborhood size
-        3,              // Aperture parameter for the Sobel operator
-        0.04);          // Harris detector free parameter
-
-    // Normalizing - Convert corner values to integer so they can be drawn
-    normalize(output, output_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
-    convertScaleAbs(output_norm, output_norm_scaled); 
-
-    // Drawing a circle around corners
-    for (int j = 0; j < output_norm.rows; j++) {
-        for (int i = 0; i < output_norm.cols; i++) {
-            if ((int)output_norm.at<float>(j, i) > 128) {
-                circle(image, Point(i, j), 4, Scalar(0, 255, 0), 2, 8, 0); //points i and j are where the croner is. 
-            }
-        }
-    }
-    // Displaying the result
-    cv::imshow("Output Harris", image);
-    cv::waitKey();
-}
-
-
-int main()
-{
     // Loading the actual image
     //image = imread("C:\\Users\\Orain\\Downloads\\first_200_right\\first_200_right\\000000.png", IMREAD_COLOR);
     image = imread("C:\\Users\\orain\\Downloads\\first_200_right\\first_200_right\\000000.png", IMREAD_COLOR);
@@ -50,6 +23,55 @@ int main()
     if (image.empty()) {
         cout << "Error loading image" << endl;
     }
+
+    //Opens orginal image
+    //cv::imshow("Original image", image);
+    // 
+    // Converting the color image into grayscale
+    cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+
+    // Detecting corners using the cornerHarris built in function
+    output = Mat::zeros(image.size(), CV_32FC1);
+    cornerHarris(gray, output,
+        2,              // blockSize
+        3,              // Aperture parameter for the Sobel operator
+        0.04);          // Harris detector free parameter
+
+    // Normalizing - Convert corner values to integer so they can be drawn
+    normalize(output, output_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
+    convertScaleAbs(output_norm, output_norm_scaled); 
+
+    vector<int> kNearestN;
+    vector<int> kNearestNOut;
+
+    // Drawing a circle around corners
+    for (int j = 0; j < output_norm.rows; j++) {
+        for (int i = 0; i < output_norm.cols; i++) {
+            if ((int)output_norm.at<float>(j, i) > 128) {
+                circle(image, Point(i, j), 4, Scalar(0, 255, 0), 2, 8, 0); //points i and j are where the corner is. 
+                kNearestN.push_back(i);
+                kNearestN.push_back(j);
+
+            }
+        }
+    }
+
+    for (const int& i : kNearestN) {
+        cout << i << "  ";
+    }
+    
+    int k = 1; 
+    cv::ml::KNearest::findNearest(kNearestN,k,kNearestNOut);
+
+
+    // Displaying the result
+    cv::imshow("Output Harris", image);
+    cv::waitKey();
+}
+
+
+int main()
+{
     harrisCornerDetector();
 
     return 0;
